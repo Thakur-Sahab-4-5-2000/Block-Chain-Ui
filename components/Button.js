91008646e7cd5f672d7aed326285a551970e51de
Thakur-Sahab-4-5-2000 <shubhamhntr159753@gmail.com>
2022-08-17 13:58:25 +0530

    Deliveroo App

---
+++ components/Button.js
@@ -0,0 +1,55 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { COLORS, SIZES, SHADOWS, assets,FONTS } from "../constants";
+import { Image } from "react-native";
+
+export const CircleButton = ({ imageUrl, handlePress, ...props }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        width: 40,
+        height: 40,
+        backgroundColor: COLORS.white,
+        position: "absolute",
+        borderRadius: SIZES.extraLarge,
+        alignItems: "center",
+        justifyContent: "center",
+        ...SHADOWS.light,
+        ...props,
+      }}
+      onPress={handlePress}
+    >
+      <Image
+        source={imageUrl}
+        resizeMode="contain"
+        style={{ width: 24, height: 24 }}
+      />
+    </TouchableOpacity>
+  );
+};
+
+export const ReactButton = ({ minWidth, fontSize, handlePress, ...props }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        backgroundColor: COLORS.primary,
+        padding: SIZES.small,
+        borderRadius: SIZES.extraLarge,
+        minWidth: minWidth,
+        ...props,
+      }}
+      onPress={handlePress}
+    >
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: fontSize,
+          color: COLORS.white,
+          textAlign: "center",
+        }}
+      >
+        Place a bid
+      </Text>
+    </TouchableOpacity>
+  );
+};
---
+++ components/index.js
@@ -0,0 +1,20 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, ReactButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import { SubInfo, EthPrice,NFTTitle } from "./SubInfo";
+
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  ReactButton,
+  DetailsBid,
+  DetailsDesc,
+  SubInfo,
+  EthPrice,
+  NFTTitle
+};
